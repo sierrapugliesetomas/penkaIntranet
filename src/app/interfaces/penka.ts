22b1b112e0e782d603cd71b805e47da0c9ef0aa3
Tomas <T.sierra@redlink.com.ar>
2021-04-23 02:02:16 -0300

    se añaden propiedades a la interfaz penka

--- src/app/interfaces/penka.ts
+++ src/app/interfaces/penka.ts
@@ -1,19 +1,52 @@
 export interface Penka {
     id?: string;
-    typePenka?: string;
-    nParticipants?: number;
-    name?: string;
-    codePenka?: string;
     makerId?: string;
     makerName?: string;
     makerEmail?: string;
-    singleMatchesId?: string[];
-    tournamentId?: string;
-    tournamentName?: string;
-    formatName?: string;
+    makerPhoto?: string;
+    name?: string;
+    type?: string; /* singleMatches, templates, tournament */
+    codePenka?: string; /* code sharing */
+    format?: string;
+    public?: boolean;
     bet?: number;
-    distributionBet?: number;
-    accumulatedBet?: number;
-    date?: string;
-    status?: string;
+    distributionBet?: string;
+    prize?: number;
+    accumulatedBet: number;
+    coin?: string; /* USD, PEN */
+    nParticipants?: number;
+    limitParticipants?: number;
+    status?: string; /* 1- in Game // 2- finished  //  3- archived  */
+    date?: Date;
+    dateLimit?: Date;
+    createdAt?: Date;
+    visibility: string;
+    typePenka: string;
+    formatName: string;
+    singleMatchesId: string[];
+    codeTemplate: string;
+    codeTournament: string;
+    tournamentId : string;
+    tournamentName : string;
 }
+
+
+
+
+// id : string;
+// typePenka : string;
+// nParticipants : number;
+// name : string;
+// codePenka : string;
+// makerId : string;
+// makerName : string;
+// makerEmail : string;
+// singleMatchesId : string[];
+// tournamentId : string;
+// tournamentName : string;
+// formatName : string;
+// bet : number;
+// distributionBet : number;
+// accumulatedBet : number;
+// date : string;
+// status : string;
